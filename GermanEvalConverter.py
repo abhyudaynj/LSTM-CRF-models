4c4eb6b88a33a8a1adcdaf47815882e1cfdb2a28
Benjamin Staude <staude@architrave.de>
2017-05-13 14:03:23 +0200

    Remove logic for test-files

--- GermanEvalConverter.py
+++ GermanEvalConverter.py
@@ -9,14 +9,22 @@ import json
 SOURCE_DIR = "data/sources/GermEval2014_complete_data"
 TARGET_DIR = "data/converted/GermEval2014_complete_data/datasets"
 SOURCE_FILE = "NER-de-train.tsv"
+BATCH_SIZE = 1000
 
+
+def write_files(sentences, annotations, filename):
+    with open(os.path.join(TARGET_DIR, filename), 'w') as txt_file:
+        txt_file.write("\n".join(sentences))
+    with open(os.path.join(TARGET_DIR, filename + '.json'), 'w', encoding='utf8') as json_file:
+        json.dump(annotations, json_file, ensure_ascii=False)
 
 def create_text_file():
     file, ext = os.path.splitext(SOURCE_FILE)
     sentences = []
     sentence = ''
     char_count = 0
-    labelId = 0
+    label_id = 0
+    batch_id = 0
     annotations = []
     doBreak = False
     with open(os.path.join(SOURCE_DIR, SOURCE_FILE), 'r') as f:
@@ -26,45 +34,28 @@ def create_text_file():
             elif len(txt) > 1:
                 txt_list = txt.split('\t')
                 word = txt_list[1]
-
                 if word == '"':  # ignore quotation marks
                     continue
 
-                # if word in ['.', ',', ':', ';', '!', '?']:
-                #     sentence += word
-                # else:
                 start = char_count + len(sentence) + 1
-                # end = start + len(word)
                 sentence += ' ' + word
                 end = char_count + len(sentence)
-                if word == 'Köpernicker':
-                    print(k, word)
-                    doBreak = True;
-
                 label = txt_list[2]
                 if label != "O":
-                    annotations.append([start, end, word, label, labelId])
-                    labelId += 1
+                    annotations.append([start, end, word, label, label_id])
+                    label_id += 1
             else:  # the sentence is over
                 sentences.append(sentence)  #
                 char_count += len(sentence) + 1 #
-
-            # if len(sentences) > 200:
-            #     file = 'test'
-            #     break
-            if doBreak:
-                sentences.append(sentence)  #
-                char_count += len(sentence) + 1  #
-                file = 'test'
-                break
+                if len(sentences) == BATCH_SIZE:
+                    write_files(sentences, annotations, file + '_' + str(batch_id))
+                    sentences = []
+                    annotations = []
+                    char_count = 0
+                    batch_id += 1
 
     sentences.append(sentence)  #
-    char_count += len(sentence) + 1  #
-    with open(os.path.join(TARGET_DIR, file), 'w') as txt_file:
-        txt_file.write("\n".join(sentences))
-
-    with open(os.path.join(TARGET_DIR, file + '.json'), 'w', encoding='utf8') as json_file:
-        json.dump(annotations, json_file, ensure_ascii=False)
+    write_files(sentences, annotations, file + '_' + str(batch_id))
 
 if __name__ == '__main__':
     create_text_file()
