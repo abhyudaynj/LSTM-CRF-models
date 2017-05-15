fa9bac51f16e3bd41e17a3af1e25aff661a1914a
Benjamin Staude <staude@architrave.de>
2017-05-15 09:31:10 +0200

    Combine single NER tags into sequence tags

--- GermanEvalConverter.py
+++ GermanEvalConverter.py
@@ -18,15 +18,15 @@ def write_files(sentences, annotations, filename):
     with open(os.path.join(TARGET_DIR, filename + '.json'), 'w', encoding='utf8') as json_file:
         json.dump(annotations, json_file, ensure_ascii=False)
 
-def create_text_file():
+
+def create_text_files():
     file, ext = os.path.splitext(SOURCE_FILE)
     sentences = []
     sentence = ''
     char_count = 0
     label_id = 0
     batch_id = 0
-    annotations = []
-    doBreak = False
+    labels = []
     with open(os.path.join(SOURCE_DIR, SOURCE_FILE), 'r') as f:
         for k, txt in enumerate(f.readlines()):
             if txt[0] == '#':  # a new sentence begins
@@ -34,28 +34,36 @@ def create_text_file():
             elif len(txt) > 1:
                 txt_list = txt.split('\t')
                 word = txt_list[1]
-                if word == '"':  # ignore quotation marks
-                    continue
+                # if word == '"':  # ignore quotation marks
+                #     continue
 
                 start = char_count + len(sentence) + 1
                 sentence += ' ' + word
                 end = char_count + len(sentence)
                 label = txt_list[2]
-                if label != "O":
-                    annotations.append([start, end, word, label, label_id])
+                if label == "O":
+                    continue
+                elif label[0:2] == 'B-':  # beginning of label
+                    label_obj = [start, end, word, label, label_id]
+                    labels.append(label_obj)
                     label_id += 1
+                else:  # intermediate label
+                    label_obj = labels[-1]
+                    label_obj[1] += len(word) + 1
+                    label_obj[2] += ' ' + word
+                    labels[-1] = label_obj
             else:  # the sentence is over
                 sentences.append(sentence)  #
                 char_count += len(sentence) + 1 #
                 if len(sentences) == BATCH_SIZE:
-                    write_files(sentences, annotations, file + '_' + str(batch_id))
+                    write_files(sentences, labels, file + '_' + str(batch_id))
                     sentences = []
-                    annotations = []
+                    labels = []
                     char_count = 0
                     batch_id += 1
 
     sentences.append(sentence)  #
-    write_files(sentences, annotations, file + '_' + str(batch_id))
+    write_files(sentences, labels, file + '_' + str(batch_id))
 
 if __name__ == '__main__':
-    create_text_file()
+    create_text_files()
