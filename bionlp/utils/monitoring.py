dd471cf57ccc645bb5df11d0bc020b35c78ff017
staude <staude@architrave.de>
2017-06-01 16:14:16 +0200

    Move monitoring to module and implement storage

--- bionlp/utils/monitoring.py
+++ bionlp/utils/monitoring.py
@@ -8,29 +8,29 @@ https://stackoverflow.com/questions/6841853/python-accessing-module-scope-vars/6
 TYPE_VALIDATION = 'validation'
 TYPE_TRAINING = 'training'
 
+KEY_ACC = 'accuracy'
+KEY_LOSS_TOT = 'loss_total'
+KEY_LOSS_CRF = 'loss_crf'
+KEY_LOSS_NET_CRF = 'loss_net_crf'
+
+
+def get_data_keys():
+    return [
+        KEY_ACC,
+        KEY_LOSS_CRF,
+        KEY_LOSS_NET_CRF,
+        KEY_LOSS_TOT
+    ]
+
 
 class MonitoringDataObject(object):
-    KEY_ACC = 'accuracy'
-    KEY_LOSS_TOT = 'loss_total'
-    KEY_LOSS_CRF = 'loss_crf'
-    KEY_LOSS_NET_CRF = 'loss_net_crf'
-
-    @staticmethod
-    def get_data_keys():
-        return [
-            MonitoringDataObject.KEY_ACC,
-            MonitoringDataObject.KEY_LOSS_CRF,
-            MonitoringDataObject.KEY_LOSS_NET_CRF,
-            MonitoringDataObject.KEY_LOSS_TOT
-        ]
-
     def __init__(self):
         super(MonitoringDataObject, self).__init__()
-        self.__data = dict((key, []) for key in self.get_data_keys())
+        self.__data = dict((key, []) for key in get_data_keys())
 
     def add_iteration_data(self, value_dict):
-        assert set(value_dict.keys()) == set(self.get_data_keys()), \
-            "input data does not contain the necessary keys {}".format(self.get_data_keys())
+        assert set(value_dict.keys()).issubset(set(get_data_keys())), \
+            "input data does not contain the necessary keys {}".format(get_data_keys())
         [self.__data[key].append(value) for key, value in value_dict.items()]
 
     def get_data(self):
