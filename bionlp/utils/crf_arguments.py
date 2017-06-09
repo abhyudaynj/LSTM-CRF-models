4d22de3375dea8dfedaa4e1ce19a043d44964cef
Matthias Fischer <mfischer1990@gmx.net>
2017-06-09 10:29:08 +0200

    Change short arg name for monitoring-file from mo to mf

--- bionlp/utils/crf_arguments.py
+++ bionlp/utils/crf_arguments.py
@@ -116,8 +116,9 @@ def crf_model_arguments():
                         default=0.0, help='Add l2 penalty. default 0.0, off')
     parser.add_argument('-mode', '--mode', dest='mode', type=int, default=1,
                         help='Mode of structured inference. Default is 1 : Modeling unary and binary potentials with neural nets. 1: Approximating Messages -1 : Only modeling the unary potential with neural nets.')
-    parser.add_argument('-mo', '--monitoring-file', type=str, dest='monitoring-file', default='None',
+    parser.add_argument('-mf', '--monitoring-file', type=str, dest='monitoring-file', default='None',
                         help='Path to the file to store monitoring during training. Default is None (no monitoring)')
+
     args = parser.parse_args()
     if args.input == 'None':
         parser.error("Input is a required argument")
